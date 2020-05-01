7d278be04ef69127da79ba422ba3640dba871f60
Xiaoshan He <40234680+xiaoshanhe003@users.noreply.github.com>
2020-04-30 20:04:40 -0400

    updated

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Copyright (c) 2014 The Regents of the University of Michigan.
-  // All Rights Reserved. Licensed according to the terms of the Revised BSD license.
-  // See LICENSE.txt for details.
-
-  require 'minified/html.php';
-?>
